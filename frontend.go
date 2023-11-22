e570dfc62046865f054b4bdf75ca99393166843e
elafontaine <eric.lafontaine1@gmail.com>
2023-11-21 20:20:34 -0500

    refactor tests

--- frontend.go
+++ frontend.go
@@ -13,6 +13,11 @@ const tpl = `
             text-decoration-color: var(--bs-gray);
         }
         </style>
+        <script>
+        $('#modal-add-task').on('shown.bs.modal', function () {
+            $('#addIcon').trigger('focus')
+          })
+        </script>
     </head>
     <body>
         {{range $i, $a := .}}
@@ -23,9 +28,24 @@ const tpl = `
         {{else}}
             <div><strong>no rows, click the + button</strong></div>
         {{end}}
-        <svg class="bi bi-plus-circle justify-content-md-end align-items-md-end" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" style="font-size: 55px;position: absolute;bottom: 25px;right: 25px;">
+        <svg id="addIcon" class="bi bi-plus-circle justify-content-md-end align-items-md-end" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" style="font-size: 55px;position: absolute;bottom: 25px;right: 25px;">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
         </svg>
+        <div id="modal-add-task" class="modal fade show" role="dialog" tabindex="-1" style="display: block;">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form id="task-form" method="POST" action="/add">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Add Task</h4><button class="btn-close" type="button" aria-label="Close" data-bs-dismiss="modal"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="d-sm-flex align-items-sm-end md-form mb-5 input-group-text align-bottom"><label class="form-label" data-error="wrong" date-success="right" for="modalTaskField">Task :</label><input id="modalTaskField" class="form-control form-control validate" type="text" value="task" /></div>
+                        </div>
+                        <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">Close</button><button class="btn btn-primary" type="submit">Save</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>`
--- main_test.go
+++ main_test.go
@@ -1,53 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"os"
 	"strings"
 	"testing"
 )
-
-func TestCreateTaskFileIfDoesNotExist(t *testing.T) {
-	fileName := "random-filename.test"
-	for i := 0; i < 2; i++ { // 0- no file, 1- file should exist and still succeed
-		testGetTasksFromFile(fileName, t)
-	}
-	err := os.Remove(fileName)
-	if err != nil {
-		t.Errorf("Couldn't delete test file : %v", err)
-	}
-}
-
-func testGetTasksFromFile(fileName string, t *testing.T) {
-	tasks, err := getTasksFromFile(fileName) 		//testee
-	if err != nil {
-		t.Error("Something very wrong happened")
-	}
-	if len(tasks) != 0 {
-		t.Error("Content of the file isn't empty!")
-	}
-}
-
-func TestCanAddTaskToTasksList(t *testing.T){
-	description := "New task to add"
-	formContent := "description="+ description
-	r := httptest.NewRequest("POST","http://example.com/tasks/",strings.NewReader(formContent))
-	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	tasks := []Task{}
-	ctx := context.WithValue(r.Context(),"tasks",&tasks)
-	r = r.WithContext(ctx)
-	output_tasks,_ := addNewTaskToAppList(r) //testee
-
-	fmt.Printf("Tasks : %v", output_tasks)
-	if !Equal(tasks, []Task{{Description: description, completed: false, hidden: false}}){
-		t.Error("Task list weren't equal")
-	}
-}
-
 
 func TestCanAddFormReturnALocationToGetRoot(t *testing.T) {
 	description := "New task to add"
@@ -55,12 +14,15 @@ func TestCanAddFormReturnALocationToGetRoot(t *testing.T) {
 	params.Add("description", description)
 	tasks := []Task{}
 
-	r := httptest.NewRequest("POST","http://example.com/tasks/", strings.NewReader(params.Encode()))
+	r := httptest.NewRequest("POST", "http://example.com/tasks/", strings.NewReader(params.Encode()))
 	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	w := httptest.NewRecorder()
-	
-	addFormFunc(&tasks)(w,r) //testee
 
+	addFormFunc(&tasks)(w, r) //testee
+
+	if !Equal(tasks, []Task{{Description: description, completed: false, hidden: false}}) {
+		t.Error("Task list weren't equal")
+	}
 	if w.Result().StatusCode != http.StatusFound {
 		t.Error("Did not receive expected redirect to root")
 	}
@@ -69,17 +31,18 @@ func TestCanAddFormReturnALocationToGetRoot(t *testing.T) {
 	}
 }
 
+func TestAddFormHasWrongInput(t *testing.T){
+	
+}
+
 func TestCanGetTasksFromUri(t *testing.T) {
-	description := "New task to add"
-	params := url.Values{}
-	params.Add("description", description)
 	tasks := []Task{}
 
-	r := httptest.NewRequest("GET","http://example.com/tasks/", strings.NewReader(params.Encode()))
+	r := httptest.NewRequest("GET", "http://example.com/tasks/", nil)
 	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	w := httptest.NewRecorder()
-	
-	addFormFunc(&tasks)(w,r) //testee
+
+	addFormFunc(&tasks)(w, r) //testee
 
 	if w.Result().StatusCode != http.StatusFound {
 		t.Error("Did not receive expected redirect to root")
